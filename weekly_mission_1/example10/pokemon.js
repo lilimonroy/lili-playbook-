d36e5aaf2e79fc333c2e68ac3514a620af055411
Lili Monroy <galadriel@comunidad.unam.mx>
2022-04-13 19:52:28 -0700

    feat: Add example 10

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,14 @@
+export default class Pokemon{
+	 constructor(name){
+    // Al crear este objeto se guardarán estos valores
+    this.name = name
+  }
+
+  sayHello(){
+  	console.log(`Mi pokemon ${this.name} te saluda!!!`)
+  }
+
+  sayMessage(msg){
+  	console.log(`Mi pokemon ${this.name} dice: $(msg)`)
+  }
+}
